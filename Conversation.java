1606ba7b7d25362144013e7def0a547f7aeac07f
Mattea29 <98430244+Mattea29@users.noreply.github.com>
2023-09-24 11:25:25 -0400

    taking user input for number of rounds and starting conversation

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 class Conversation {
 
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    System.out.println("Let's talk!");
+    Scanner input = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    int round = input.nextInt();
+    if (round == 1) {
+      System.out.println("Ok! Let's talk for " + round + " round.");
+    } else {
+      System.out.println("Ok! Let's talk for " + round + " rounds.");
+    };
+    System.out.println("What's on your mind?");
+
   }
 }
